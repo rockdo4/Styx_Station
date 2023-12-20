f513e39ac04f8b98401a9e4708ae561524acde54
이승우 <chrisnarulee123@gmail.com>
2023-12-20 15:39:20 +0900

    뱀서 연구중 플레이어 강화 UI 버그빠진부분이 있어서 추가함 ,

--- Styx_Station/Assets/03. Scripts/Player/ResultPlayerStats.cs
+++ Styx_Station/Assets/03. Scripts/Player/ResultPlayerStats.cs
@@ -88,7 +88,7 @@ public class ResultPlayerStats : MonoBehaviour
     }
     public BigInteger GetPlayerPowerByNonInventory()
     {
-        return (int)playerAttribute.attackPower + (SharedPlayerStats.GetPlayerPower() - 1);
+        return (int)playerAttribute.attackPower + (SharedPlayerStats.GetPlayerPower() - 1 * increaseUpgradePower);
     }
     public BigInteger GetPlayerPower() 
     {
--- Styx_Station/Assets/03. Scripts/VampireSurvival/Enemy/VampireSurivalMonster.cs
+++ Styx_Station/Assets/03. Scripts/VampireSurvival/Enemy/VampireSurivalMonster.cs
@@ -6,9 +6,35 @@ public class VampireSurivalMonster : PoolAble
 {
     public int maxHp;
     public int hp;
+    public float speed;
     private float timer;
     public float findPlayerTimer;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    private VamprieSurivalPlayerController player;
 
+
+    private void Awake()
+    {
+        rb =GetComponent<Rigidbody2D>();
+    }
+
+    public void Start()
+    {
+        maxHp = 2 + (1-1); // 추후 현재 웨이브로 -1
+        if(player ==null)
+        {
+            player = GameObject.FindWithTag("VampirePlayer").GetComponent<VamprieSurivalPlayerController>();
+        }
+        FindPlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        var velocity = rb.velocity;
+        velocity += direction * speed * Time.fixedDeltaTime;
+        rb.velocity = velocity;
+    }
 
     public void Update()
     {
@@ -21,6 +47,6 @@ public class VampireSurivalMonster : PoolAble
     }
     public void FindPlayer()
     {
-
+        direction = (player.transform.position - gameObject.transform.position).normalized;
     }
 }

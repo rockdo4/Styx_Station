e4ab29cc53097441ba91d2da42738cf087d05828
윤유림 <0232dbfla@naver.com>
2024-01-17 16:17:20 +0900

    방치 보상 변경, 스테이지 수정

--- Styx_Station/Assets/03. Scripts/ScriptableObject/Stage.cs
+++ Styx_Station/Assets/03. Scripts/ScriptableObject/Stage.cs
@@ -14,14 +14,14 @@ public class Stage : ScriptableObject
     public int monsterAttackIncrease;
     public int monsterHealthIncrease;
     public float monsterAttackSpeedIncrease;
-    public MonsterTypeBase monster1;
-    public int monster1Count;
-    public MonsterTypeBase monster2;
-    public int monster2Count;
-    public MonsterTypeBase monster3;
-    public int monster3Count;
-    public MonsterTypeBase monster4;
-    public int monster4Count;
+    //public MonsterTypeBase monster1;
+    //public int monster1Count;
+    //public MonsterTypeBase monster2;
+    //public int monster2Count;
+    //public MonsterTypeBase monster3;
+    //public int monster3Count;
+    //public MonsterTypeBase monster4;
+    //public int monster4Count;
 
     public List<MonsterTypeBase> monsterList;
     public List<int> monsterCountList;
